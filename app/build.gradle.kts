9070d17325c5de1e6f7af33b49509e5fa1fe5441
KonjikiNoYami <zidanalbani97gmail.com>
2026-06-14 16:10:11 +0700

    bump version to 1.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.silica.assistant"
         minSdk = 24
         targetSdk = 34
-        versionCode = 6
-        versionName = "1.3"
+        versionCode = 7
+        versionName = "1.4"
         
         val localProperties = Properties()
         val localPropertiesFile = rootProject.file("local.properties")
